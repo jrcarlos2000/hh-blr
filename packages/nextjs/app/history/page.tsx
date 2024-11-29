fd0bc607cdb3629323ad0c8a9b0926ef6aea5145
ngjupeng <jupeng2015@gmail.com>
2024-11-29 14:00:00 +0800

    feat: account overview hook + swap&approve history

--- packages/nextjs/app/history/page.tsx
+++ packages/nextjs/app/history/page.tsx
@@ -2,14 +2,12 @@
 
 import React from "react";
 import { Header } from "~~/components/Header";
+import { useAccountOverview } from "~~/hooks/useAccountBalanceOverview";
 import { useTransactionHistory } from "~~/hooks/useTransactionHistory";
 
 const History = () => {
-  const { history } = useTransactionHistory([
-    "0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d",
-    "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7",
-  ]);
-  console.log(history);
+  const { overview, loading, error, refetch } = useAccountOverview();
+  console.log(overview);
   return (
     <div>
       <Header />
--- packages/nextjs/hooks/useTransactionHistory.ts
+++ packages/nextjs/hooks/useTransactionHistory.ts
@@ -6,66 +6,130 @@ import {
   hash,
   uint256,
   getChecksumAddress,
+  TransactionReceipt,
+  GetTransactionReceiptResponse,
 } from "starknet";
-import { universalErc20Abi } from "~~/utils/Constants";
+import { sepoliaAvnuAddress, universalErc20Abi } from "~~/utils/Constants";
 import { EmittedEvent } from "@starknet-io/types-js/dist/types/api";
 import { formatEther } from "ethers";
 import { useInterval } from "usehooks-ts";
+import { TransactionWithHash } from "@starknet-io/types-js/dist/types/api/nonspec";
+import { useTargetNetwork } from "./scaffold-stark/useTargetNetwork";
 
 interface TransactionEvent {
-  type: "send" | "receive";
-  tokenAddress: string;
-  amount: string;
-  from: string;
-  to: string;
+  type: "send" | "receive" | "approve" | "swap";
+  contractAddress: string;
+  amount?: string;
+  from?: string;
+  to?: string;
   //   timestamp: number;
   hash: string;
+  swap?: {
+    taker_address: string;
+    sell_address: string;
+    sell_amount: string;
+    buy_address: string;
+    buy_amount: string;
+    beneficiary: string;
+  };
+  receipt?: any;
 }
 
-export const useTransactionHistory = (tokenAddresses: string[]) => {
+export const useTransactionHistory = (contractAddresses: string[]) => {
   const { address } = useAccount();
   const [history, setHistory] = useState<TransactionEvent[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
 
-  const provider = useMemo(
-    () =>
-      new RpcProvider({
-        nodeUrl: `https://starknet-sepolia.public.blastapi.io/rpc/v0_7`,
-      }),
-    []
-  );
+  const { targetNetwork } = useTargetNetwork();
 
+  const publicNodeUrl = targetNetwork.rpcUrls.public.http[0];
+
+  const publicClient = useMemo(() => {
+    return new RpcProvider({
+      nodeUrl: publicNodeUrl,
+    });
+  }, [publicNodeUrl]);
   const fetchTransactionHistory = async () => {
-    if (!address || tokenAddresses.length === 0) return;
+    if (!address || contractAddresses.length === 0) return;
     try {
       setLoading(true);
       const allEvents: TransactionEvent[] = [];
 
-      for (const tokenAddress of tokenAddresses) {
-        const blockNumber = (await provider.getBlockLatestAccepted())
-          .block_number;
+      const blockNumber = (await publicClient.getBlockLatestAccepted())
+        .block_number;
 
+      for (const contractAddress of contractAddresses) {
         const [sentEvents] = await Promise.all([
-          provider.getEvents({
+          publicClient.getEvents({
+            from_block: { block_number: blockNumber - 50 },
+            to_block: { block_number: blockNumber },
+            chunk_size: 1000,
+            keys: [[hash.getSelectorFromName("Transfer")]],
+            address: contractAddress,
+          }),
+        ]);
+
+        const [approvalEvents] = await Promise.all([
+          publicClient.getEvents({
             from_block: { block_number: blockNumber - 50 },
             to_block: { block_number: blockNumber },
             chunk_size: 1000,
             keys: [
-              [hash.getSelectorFromName("Transfer")], // Transfer events
+              //   [hash.getSelectorFromName("Transfer")], // Transfer events
+              [hash.getSelectorFromName("Approval")], // Swap events
             ],
-            address: tokenAddress,
+            address: contractAddress,
+          }),
+        ]);
+
+        //   get swap events
+        const [swapEvents] = await Promise.all([
+          publicClient.getEvents({
+            from_block: { block_number: blockNumber - 50 },
+            to_block: { block_number: blockNumber },
+            chunk_size: 1000,
+            keys: [
+              [hash.getSelectorFromName("Swap")], // Transfer events
+            ],
+            address: sepoliaAvnuAddress,
           }),
         ]);
 
         //   Process sent events
         const processedSentEvents = await processEvents(
           sentEvents.events,
-          tokenAddress,
-          address
+          contractAddress,
+          address,
+          "send"
         );
 
-        allEvents.push(...processedSentEvents);
+        const processedApproveEvents = await processEvents(
+          approvalEvents.events,
+          contractAddress,
+          address,
+          "approve"
+        );
+
+        const processedSwapEvents = await processEvents(
+          swapEvents.events,
+          contractAddress,
+          address,
+          "swap"
+        );
+
+        allEvents.push(
+          ...processedSentEvents,
+          ...processedApproveEvents,
+          ...processedSwapEvents
+        );
+
+        // then do transaction receipt here
+
+        for (const event of allEvents) {
+          const receipt = await publicClient.getTransactionReceipt(event.hash);
+          event.receipt = receipt;
+        }
       }
 
       setHistory(allEvents);
@@ -79,7 +143,7 @@ export const useTransactionHistory = (tokenAddresses: string[]) => {
 
   useEffect(() => {
     fetchTransactionHistory();
-  }, [address, JSON.stringify(tokenAddresses)]);
+  }, [address, JSON.stringify(contractAddresses)]);
 
   useInterval(async () => {
     fetchTransactionHistory();
@@ -90,8 +154,9 @@ export const useTransactionHistory = (tokenAddresses: string[]) => {
 
 async function processEvents(
   events: EmittedEvent[],
-  tokenAddress: string,
-  accountAddress: string
+  contractAddress: string,
+  accountAddress: string,
+  type: "send" | "approve" | "swap"
 ): Promise<TransactionEvent[]> {
   const processedEvents: TransactionEvent[] = [];
 
@@ -102,22 +167,69 @@ async function processEvents(
       const userAddress = getChecksumAddress(accountAddress);
       if (fromAddress !== userAddress && toAddress !== userAddress) continue;
 
-      processedEvents.push({
-        type: fromAddress === userAddress ? "send" : "receive",
-        tokenAddress,
-        amount: formatEther(
-          uint256
-            .uint256ToBN({
-              low: event.data[2],
-              high: event.data[3],
-            })
-            .toString()
-        ),
-        from: event.data[0],
-        to: event.data[1],
-        // timestamp: Number(event.),
-        hash: event.transaction_hash,
-      });
+      if (type === "send") {
+        processedEvents.push({
+          type: fromAddress === userAddress ? "send" : "receive",
+          contractAddress,
+          amount: formatEther(
+            uint256
+              .uint256ToBN({
+                low: event.data[2],
+                high: event.data[3],
+              })
+              .toString()
+          ),
+          from: event.data[0],
+          to: event.data[1],
+          // timestamp: Number(event.),
+          hash: event.transaction_hash,
+        });
+      } else if (type === "approve") {
+        processedEvents.push({
+          type: "approve",
+          contractAddress,
+          amount: formatEther(
+            uint256
+              .uint256ToBN({
+                low: event.data[2],
+                high: event.data[3],
+              })
+              .toString()
+          ),
+          from: event.data[0],
+          to: event.data[1],
+          // timestamp: Number(event.),
+          hash: event.transaction_hash,
+        });
+      } else if (type === "swap") {
+        processedEvents.push({
+          type: "swap",
+          contractAddress: contractAddress,
+          hash: event.transaction_hash,
+          swap: {
+            taker_address: event.data[0],
+            sell_address: event.data[1],
+            sell_amount: formatEther(
+              uint256
+                .uint256ToBN({
+                  low: event.data[2],
+                  high: event.data[3],
+                })
+                .toString()
+            ),
+            buy_address: event.data[4],
+            buy_amount: formatEther(
+              uint256
+                .uint256ToBN({
+                  low: event.data[5],
+                  high: event.data[6],
+                })
+                .toString()
+            ),
+            beneficiary: event.data[7],
+          },
+        });
+      }
     } catch (err) {
       console.error("Error processing event:", err);
     }
